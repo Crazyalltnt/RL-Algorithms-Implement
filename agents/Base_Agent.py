f1bb70901ee50b1891bf4e14f66d6691e5789dcb
Crazyalltnt <ningtaook.66@gmail.com>
2021-08-18 20:17:06 +0800

    智能体基类

---
+++ agents/Base_Agent.py
@@ -0,0 +1,371 @@
+import logging
+import os
+import sys
+import gym
+import random
+import numpy as np
+import torch
+import time
+# import tensorflow as
+# from tensorboardX import SummaryWriter
+from nn_builder.pytorch.NN import NN
+from torch.optim import optimizer
+
+class Base_Agent(object):
+    def __init__(self, config) -> None:
+        self.logger = self.setup_logger()  # 设置日志管理器
+        self.debug_mode = config.debug_mode  # 调试模式
+        # if self.debug_mode:
+        #     self.tensorflow = SummaryWriter()
+        self.config = config  # 配置数据
+        self.set_random_seeds(config.seed)  # 设置随机种子
+        self.environment = config.environment  # 设置环境实例
+        self.environment_title = self.get_environment_title()  # 获取环境名
+        self.action_types = "DISCRETE" if self.environment.action_space.dtype == np.int64 else "CONTINUOUS"  # 动作状态
+        self.action_size = int(self.get_action_size())  # 获取环境动作空间大小
+        self.config.action_size = self.action_size  # 设置动作空间大小
+
+        self.lowest_possible_episode_score = self.get_lowest_possible_episode_score()  # 设置最低分
+
+        self.state_size = int(self.get_state_size())  # 获取环境状态空间大小
+        self.hyperparameters = config.hyperparameters
+        self.average_score_required_to_win = self.get_score_required_to_win()  # 设置获胜平均分
+        self.rolling_score_window = self.get_trials()  # 计算滚动平均分数的实验次数
+        # self.max_steps_per_episode = self.environment.spec.max_episode_steps
+        self.total_episode_score_so_far = 0  # 当前episode目前为止的总分
+        self.game_full_episode_scores = []  # 游戏全部episodes得分
+        self.rolling_results = []  # 滚动结果,最近n次平均值，n=rolling_score_window
+        self.max_rolling_score_seen = float("-inf")  # 最大滚动平均分数
+        self.max_episode_score_seen = float("-inf")  # 最大episode分数
+        self.episode_number = 0  # 当前episode轮次
+        self.device = "cuda:0" if config.use_GPU else "cpu"  # GPU开关
+        self.visualise_results_boolean = config.visualise_individual_results  # 结果可视化开关
+        self.global_step_number = 0  # 总训练步数
+        self.turn_off_exploration = False
+        gym.logger.set_level(40)  # 阻止打印不必要的警告
+        self.log_game_info()  # 
+
+
+    def step(self):
+        """在游戏中执行一步。 此方法必须被所有智能体重写"""
+        raise ValueError("Step needs to be implemented by the agent")
+
+    def get_environment_title(self):
+        """获取环境名称"""
+        try:
+            name = self.environment.unwrapped.id
+        except AttributeError:
+            try:
+                if str(self.environment.unwrapped)[1:11] == "FetchReach": return "FetchReach"
+                elif str(self.environment.unwrapped)[1:8] == "AntMaze": return "AntMaze"
+                elif str(self.environment.unwrapped)[1:7] == "Hopper": return "Hopper"
+                elif str(self.environment.unwrapped)[1:9] == "Walker2d": return "Walker2d"
+                else:
+                    name = self.environment.spec.id.split("-")[0]
+            except AttributeError:
+                name = str(self.environment.env)
+                if name[0:10] == "TimeLimit<": name = name[10:]
+                name = name.split(" ")[0]
+                if name[0] == "<": name = name[1:]
+                if name[-3:] == "Env": name = name[:-3]
+        return name
+
+    def get_lowest_possible_episode_score(self):
+        """返回环境中在一个episode中可能获得的最低分数"""
+        if self.environment_title == "Taxi": return -800
+        return None
+
+    def get_action_size(self):
+        """获取动作空间大小"""
+        if "overwrite_action_size" in self.config.__dict__: return self.config.overwrite_action_size
+        if "action_size" in self.environment.__dict__: return self.environment.action_size
+        if self.action_types == "DISCRETE": return self.environment.action_space.n
+        else: return self.environment.action_space.shape[0]
+
+    def get_state_size(self):
+        """获取状态空间大小"""
+        random_state = self.environment.reset()
+        if isinstance(random_state, dict):
+            state_size = random_state["observation"].shape[0] + random_state["desired_goal"].shape[0]
+            return state_size
+        else:
+            return random_state.size
+
+    def get_score_required_to_win(self):
+        """获取赢得比赛所需的平均分数"""
+        print("TITLE ", self.environment_title)
+        if self.environment_title == "FetchReach": return -5
+        if self.environment_title in ["AntMaze", "Hopper", "Walker2d"]:
+            print("Score required to win set to infinity therefore no learning rate annealing will happen")
+            return float("inf")
+        try: return self.environment.unwrapped.reward_threshold
+        except AttributeError:
+            try:
+                return self.environment.spec.reward_threshold
+            except AttributeError:
+                return self.environment.unwrapped.spec.reward_threshold
+
+    def get_trials(self):
+        """计算平均分数的实验次数"""
+        if self.environment_title in ["AntMaze", "FetchReach", "Hopper", "Walker2d", "CartPole"]: return 100
+        try: return self.environment.unwrapped.trials
+        except AttributeError: return self.environment.spec.trials
+
+    def setup_logger(self):
+        """设置logger"""
+        filename = "Training.log"
+        try: 
+            if os.path.isfile(filename): 
+                os.remove(filename)
+        except: pass
+
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.INFO)
+        # 日志句柄
+        handler = logging.FileHandler(filename)
+        handler.setLevel(logging.INFO)
+        # 日志格式
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        # 将句柄加入日志管理器
+        logger.addHandler(handler)
+        return logger
+
+    def log_game_info(self):
+        """记录与游戏相关的信息"""
+        for ix, param in enumerate([self.environment_title, self.action_types, self.action_size, self.lowest_possible_episode_score,
+                      self.state_size, self.hyperparameters, self.average_score_required_to_win, self.rolling_score_window,
+                      self.device]):
+            self.logger.info("{} -- {}".format(ix, param))
+
+    def set_random_seeds(self, random_seed):
+        """设置所有可能的随机种子，以便可以重现结果"""
+        os.environ['PYTHONHASHSEED'] = str(random_seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+        torch.manual_seed(random_seed)
+        # tf.set_random_seed(random_seed)
+        random.seed(random_seed)
+        np.random.seed(random_seed)
+        if torch.cuda.is_available():
+            torch.cuda.manual_seed_all(random_seed)
+            torch.cuda.manual_seed(random_seed)
+        if hasattr(gym.spaces, 'prng'):
+            gym.spaces.prng.seed(random_seed)
+
+    def reset_game(self):
+        """重置游戏信息，开始新的episode"""
+        self.environment.seed(self.config.seed)
+        self.state = self.environment.reset()
+        self.next_state = None
+        self.action = None
+        self.reward = None
+        self.done = False
+        self.total_episode_score_so_far = 0
+        self.episode_states = []
+        self.episode_rewards = []
+        self.episode_actions = []
+        self.episode_next_states = []
+        self.episode_dones = []
+        self.episode_desired_goals = []
+        self.episode_achieved_goals = []
+        self.episode_observations = []
+        if "exploration_strategy" in self.__dict__.keys(): self.exploration_strategy.reset()
+        self.logger.info("Reseting game -- New start state {}".format(self.state))
+
+    def track_episodes_data(self):
+        """保存最近episode的数据"""
+        self.episode_states.append(self.state)
+        self.episode_actions.append(self.action)
+        self.episode_rewards.append(self.reward)
+        self.episode_next_states.append(self.next_state)
+        self.episode_dones.append(self.done)
+
+    def run_n_episodes(self, num_episodes=None, show_whether_achieved_goal=True, save_and_print_results=True):
+        """运行游戏n次，然后总结结果并保存模型（如果要求）"""
+        if num_episodes is None: num_episodes = self.config.num_episodes_to_run
+        start = time.time()
+        while self.episode_number < num_episodes:
+            self.reset_game()
+            self.step()
+            if save_and_print_results: self.save_and_print_result()
+        time_taken = time.time() - start  # 用时
+        if show_whether_achieved_goal: self.show_whether_achieved_goal()
+        if self.config.save_model: self.locally_save_policy()  # 保存模型
+        return self.game_full_episode_scores, self.rolling_results, time_taken
+
+    def conduct_action(self, action):
+        """在环境中执行动作"""
+        self.next_state, self.reward, self.done, _ = self.environment.step(action)
+        self.total_episode_score_so_far += self.reward
+        if self.hyperparameters["clip_rewards"]: self.reward =  max(min(self.reward, 1.0), -1.0)  # 奖励剪裁
+
+
+    def save_and_print_result(self):
+        """保存并打印游戏结果"""
+        self.save_result()
+        self.print_rolling_result()
+
+    def save_result(self):
+        """保存每个episode的结果"""
+        self.game_full_episode_scores.append(self.total_episode_score_so_far)  # 添加当前episode的分数到分数列表
+        self.rolling_results.append(np.mean(self.game_full_episode_scores[-1 * self.rolling_score_window:]))  # 最后n个episode的平均分
+        self.save_max_result_seen()
+
+    def save_max_result_seen(self):
+        """更新当前为止看到的最佳episode结果"""
+        if self.game_full_episode_scores[-1] > self.max_episode_score_seen:
+            self.max_episode_score_seen = self.game_full_episode_scores[-1]
+
+        if self.rolling_results[-1] > self.max_rolling_score_seen:
+            if len(self.rolling_results) > self.rolling_score_window:
+                self.max_rolling_score_seen = self.rolling_results[-1]
+
+    def print_rolling_result(self):
+        """滚动打印最新的episode结果"""
+        text = """"\r Episode {0}, Score: {3: .2f}, Max score seen: {4: .2f}, Rolling score: {1: .2f}, Max rolling score seen: {2: .2f}"""
+        sys.stdout.write(text.format(len(self.game_full_episode_scores), self.rolling_results[-1], self.max_rolling_score_seen,
+                                     self.game_full_episode_scores[-1], self.max_episode_score_seen))
+        sys.stdout.flush()
+
+    def show_whether_achieved_goal(self):
+        """打印智能体是否达到环境目标"""
+        index_achieved_goal = self.achieved_required_score_at_index()
+        print(" ")
+        if index_achieved_goal == -1:  # 智能体从未达到目标
+            print("\033[91m" + "\033[1m" +
+                  "{} did not achieve required score \n".format(self.agent_name) +
+                  "\033[0m" + "\033[0m")
+        else:
+            print("\033[92m" + "\033[1m" +
+                  "{} achieved required score at episode {} \n".format(self.agent_name, index_achieved_goal) +
+                  "\033[0m" + "\033[0m")
+
+    def achieved_required_score_at_index(self):
+        """返回智能体实现目标的episode，如果不存在，返回-1"""
+        for ix, score in enumerate(self.rolling_results):
+            if score > self.average_score_required_to_win:
+                return ix
+        return -1
+
+    def update_learning_rate(self, starting_lr,  optimizer):
+        """根据与目标的接近程度降低学习率"""
+        if len(self.rolling_results) > 0:
+            last_rolling_score = self.rolling_results[-1]
+            if last_rolling_score > 0.75 * self.average_score_required_to_win:
+                new_lr = starting_lr / 100.0
+            elif last_rolling_score > 0.6 * self.average_score_required_to_win:
+                new_lr = starting_lr / 20.0
+            elif last_rolling_score > 0.5 * self.average_score_required_to_win:
+                new_lr = starting_lr / 10.0
+            elif last_rolling_score > 0.25 * self.average_score_required_to_win:
+                new_lr = starting_lr / 2.0
+            else:
+                new_lr = starting_lr
+            for g in optimizer.param_groups:
+                g['lr'] = new_lr
+        if random.random() < 0.001: self.logger.info("Learning rate {}".format(new_lr))
+
+    def enough_experiences_to_learn_from(self):
+        """记忆缓冲区中是否有足够的经验可供学习"""
+        return len(self.memory) > self.hyperparameters["batch_size"]
+
+    def save_experience(self, memory=None, experience=None):
+        """将最近的经验保存到记忆缓冲区"""
+        if memory is None: memory = self.memory
+        if experience is None: experience = self.state, self.action, self.reward, self.next_state, self.done
+        memory.add_experience(*experience)
+
+    def take_optimisation_step(self, optimizer, network, loss, clipping_norm=None, retain_graph=False):
+        """计算loss梯度优化更新参数"""
+        if not isinstance(network, list): network = [network]
+        optimizer.zero_grad()  # 梯度重置为0
+        loss.backward(retain_graph=retain_graph)  # 反向传播计算梯度
+        self.logger.info("Loss -- {}".format(loss.item()))
+        if self.debug_mode: self.log_gradient_and_weight_information(network, optimizer)
+        if clipping_norm is not None:
+            for net in network:
+                torch.nn.utils.clip_grad_norm_(net.parameters(), clipping_norm) #clip gradients to help stabilise training
+        optimizer.step() #this applies the gradients
+
+    def log_gradient_and_weight_information(self, network, optimizer):
+        """记录梯度和权重信息"""
+        total_norm = 0
+        for name, param in network.named_parameters():
+            param_norm = param.grad.data.norm(2)
+            total_norm += param_norm.item() ** 2
+        total_norm = total_norm ** (1. / 2)
+        self.logger.info("Gradient Norm {}".format(total_norm))
+
+        for g in optimizer.param_groups:
+            learning_rate = g['lr']
+            break
+        self.logger.info("Learning Rate {}".format(learning_rate))
+
+
+    def soft_update_of_target_network(self, local_model, target_model, tau):
+        """在原始网络的方向上更新目标网络，但步长小于原始网络，因此目标网络的参数值落后于原始网络，这有助于稳定训练"""
+        for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
+            target_param.data.copy_(tau * local_param.data + (1.0-tau) * target_param.data)
+
+    def create_NN(self, input_dim, output_dim, key_to_use=None, override_seed=None, hyperparameters=None):
+        """创建智能体神经网络"""
+        if hyperparameters is None: hyperparameters = self.hyperparameters
+        if key_to_use: hyperparameters = hyperparameters[key_to_use]
+        if override_seed: seed = override_seed
+        else: seed = self.config.seed
+
+        default_hyperparameter_choices = {"output_activation": None, "hidden_activations": "relu", "dropout": 0.0,
+                                          "initialiser": "default", "batch_norm": False,
+                                          "columns_of_data_to_be_embedded": [],
+                                          "embedding_dimensions": [], "y_range": ()}
+
+        for key in default_hyperparameter_choices:
+            if key not in hyperparameters.keys():
+                hyperparameters[key] = default_hyperparameter_choices[key]
+
+        return NN(input_dim=input_dim, layers_info=hyperparameters["linear_hidden_units"] + [output_dim],
+                  output_activation=hyperparameters["final_layer_activation"],
+                  batch_norm=hyperparameters["batch_norm"], dropout=hyperparameters["dropout"],
+                  hidden_activations=hyperparameters["hidden_activations"], initialiser=hyperparameters["initialiser"],
+                  columns_of_data_to_be_embedded=hyperparameters["columns_of_data_to_be_embedded"],
+                  embedding_dimensions=hyperparameters["embedding_dimensions"], y_range=hyperparameters["y_range"],
+                  random_seed=seed).to(self.device)
+
+    def turn_on_any_epsilon_greedy_exploration(self):
+        """打开epsilon-greedy探索"""
+        print("Turning on epsilon greedy exploration")
+        self.turn_off_exploration = False
+
+    def turn_off_any_epsilon_greedy_exploration(self):
+        """关闭epsilon-greedy探索"""
+        print("Turning off epsilon greedy exploration")
+        self.turn_off_exploration = True
+
+    def freeze_all_but_output_layers(self, network):
+        """冻结网络输出层以外的所有层"""
+        print("Freezing hidden layers")
+        for param in network.named_parameters():
+            param_name = param[0]
+            assert "hidden" in param_name or "output" in param_name or "embedding" in param_name, "Name {} of network layers not understood".format(param_name)
+            if "output" not in param_name:
+                param[1].requires_grad = False
+
+    def unfreeze_all_layers(self, network):
+        """解冻网络所有层"""
+        print("Unfreezing all layers")
+        for param in network.parameters():
+            param.requires_grad = True
+
+    @staticmethod
+    def move_gradients_one_model_to_another(from_model, to_model, set_from_gradients_to_zero=False):
+        """从一个模型拷贝梯度到另一个模型"""
+        for from_model, to_model in zip(from_model.parameters(), to_model.parameters()):
+            to_model._grad = from_model.grad.clone()
+            if set_from_gradients_to_zero: from_model._grad = None
+
+    @staticmethod
+    def copy_model_over(from_model, to_model):
+        """从一个模型拷贝参数到另一个模型"""
+        for to_model, from_model in zip(to_model.parameters(), from_model.parameters()):
+            to_model.data.copy_(from_model.data.clone())
